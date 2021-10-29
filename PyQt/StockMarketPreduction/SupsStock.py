a890f80498180d0041fdc703620a1ae6897a6f21
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-29 14:08:04 -0700

    Update SupsStock.py
    Added listing stock tickers to dropdown menu

--- PyQt/StockMarketPreduction/SupsStock.py
+++ PyQt/StockMarketPreduction/SupsStock.py
@@ -5,16 +5,19 @@ import scipy.special as sc
 from scipy import optimize
 import csv, math
 import pyqtgraph as pg
+from pyqtgraph import DateAxisItem
 import StockGUI
 import sys
 import math
 import pandas_datareader as web
+from yahoo_fin import stock_info as si
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
+# from sklearn.preprocessing import MinMaxScaler
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 
@@ -22,7 +25,8 @@ class MainClass(QDialog,StockGUI.Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
+        self.listStock()        #Load tickers to combobox
+
         ###################Stryling Graph###########################
         label_style = {'color': '#EEE', 'font-size': '10pt'}
         self.graph = pg.PlotWidget(title="Stock Market")
@@ -49,6 +53,7 @@ class MainClass(QDialog,StockGUI.Ui_Dialog):
 
         self.pushButton_search_ss.clicked.connect(self.showStock)
 
+    # Display Stock Chart
     def showStock(self):
         df = web.DataReader('TSLA', data_source='yahoo', start = '2020-01-01', end='2021-10-28')
         print(df.index)
@@ -59,8 +64,35 @@ class MainClass(QDialog,StockGUI.Ui_Dialog):
         # listdate2 = pd.date_range(start="2020-01-01",end="2021-10-25")
         # print(listdate2)
 
+    # Collecting all current list of tickers from yahoo financial
+    def listStock(self):
+        # gather stock symbols from major US exchanges
+        df1 = pd.DataFrame( si.tickers_sp500() )
+        df2 = pd.DataFrame( si.tickers_nasdaq() )
+        df3 = pd.DataFrame( si.tickers_dow() )
+        df4 = pd.DataFrame( si.tickers_other() )
 
+        # convert DataFrame to list, then to sets
+        sym1 = set( symbol for symbol in df1[0].values.tolist() )
+        sym2 = set( symbol for symbol in df2[0].values.tolist() )
+        sym3 = set( symbol for symbol in df3[0].values.tolist() )
+        sym4 = set( symbol for symbol in df4[0].values.tolist() )
 
+        # join the 4 sets into one. Because it's a set, there will be no duplicate symbols
+        symbols = set.union( sym1, sym2, sym3, sym4 )
+
+        # Some stocks are 5 characters. Those stocks with the suffixes listed below are not of interest.
+        my_list = ['W', 'R', 'P', 'Q']
+        del_set = set()
+        sav_set = set()
+
+        for symbol in symbols:
+            if len( symbol ) > 4 and symbol[-1] in my_list:
+                del_set.add( symbol )
+            else:
+                sav_set.add( symbol )
+        for ticker in sav_set:
+            self.comboBox_ticker.addItem(str(ticker))
 
 
 if __name__=='__main__':
