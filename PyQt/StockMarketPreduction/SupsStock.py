67eadb1c3bd7966a02fb66cd20ebe6690002478b
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-28 17:04:25 -0700

    Update SupsStock.py
    Plotting Stock market

--- PyQt/StockMarketPreduction/SupsStock.py
+++ PyQt/StockMarketPreduction/SupsStock.py
@@ -22,6 +22,42 @@ class MainClass(QDialog,StockGUI.Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        
+        ###################Stryling Graph###########################
+        label_style = {'color': '#EEE', 'font-size': '10pt'}
+        self.graph = pg.PlotWidget(title="Stock Market")
+        self.graph.showGrid(x = True, y = True, alpha = 0.3)
+        self.graph.setLabel('left', "USD ($)")
+        self.graph.getAxis('left').setLabel(**label_style)
+        self.graph.getAxis('bottom').setLabel(**label_style)
+        self.graph.setLabel('bottom', "Period (Year)")
+
+        font=QtGui.QFont()
+        font.setPixelSize(15)
+        self.graph.getAxis("bottom").tickFont = font
+        # self.graph.getAxis("bottom").setStyle(tickTextOffset=20)
+        self.graph.getAxis("left").tickFont = font
+        # self.graph.getAxis("left").setStyle(tickTextOffset=20)
+        # self.graph.setYRange(-120,10)
+
+        self.gridLayout_graph.addWidget(self.graph, 0, 0)
+        self.plot1 = self.graph.plot(pen='r')
+        self.plot2 = self.graph.plot(pen='b')
+        self.plot3 = self.graph.plot()
+        self.plot1.show()
+
+
+        self.pushButton_search_ss.clicked.connect(self.showStock)
+
+    def showStock(self):
+        df = web.DataReader('TSLA', data_source='yahoo', start = '2020-01-01', end='2021-10-28')
+        print(df.index)
+        self.plot1.setData(df['Close'])
+        self.plot2.setData(df['Open'])
+        self.plot1.show()
+        self.plot2.show()
+        # listdate2 = pd.date_range(start="2020-01-01",end="2021-10-25")
+        # print(listdate2)
 
 
 
